5eeb8c0a2d8fe83f4ed854a89eee52af6cec4782
Mason Embry <mason@embrycode.com>
2021-01-02 07:39:12 -0600

    Improve styling

--- index.php
+++ index.php
@@ -3,32 +3,35 @@ include('db.php');
 include('html/topHTML.php');
 ?>
 
-<h1>All recipes</h1>
+<main>
+    <h1>All recipes</h1>
 
-<?php
-$db = new db();
-$allRecipes = $db->getAllRecipes();
+    <div class="recipe-container">
+        <?php
+        $db = new db();
+        $allRecipes = $db->getAllRecipes();
 
-foreach ($allRecipes as $recipe) {
-    $ingredients = $db->getIngredientsByRecipeId($recipe['id'])
+        foreach ($allRecipes as $recipe) {
+            $ingredients = $db->getIngredientsByRecipeId($recipe['id'])
 
-    ?>
-    <div class="recipe-card">
-        <h2><?php echo ucfirst($recipe['name']) ?></h2>
-        <h3>Ingredients</h3>
-        <ul>
-            <?php foreach ($ingredients as $ingredient) { ?>
-                <li><?php echo $ingredient['name'] ?></li>
-            <?php } ?>
-        </ul>
+            ?>
+            <div class="recipe-card">
+                <h2><?php echo ucfirst($recipe['name']) ?></h2>
+                <h3>Ingredients</h3>
+                <ul>
+                    <?php foreach ($ingredients as $ingredient) { ?>
+                        <li><?php echo $ingredient['name'] ?></li>
+                    <?php } ?>
+                </ul>
 
-        <h3>Instructions</h3>
-        <p><?php echo $recipe['instructions'] ?></p>
+                <h3>Instructions</h3>
+                <p><?php echo $recipe['instructions'] ?></p>
 
-        <a href="/add-edit-recipe.php?recipe-id=<?php echo $recipe['id'] ?>">Edit</a> | <a href="/delete-recipe.php?recipe-id=<?php echo $recipe['id'] ?>">Delete</a>
+                <a href="/add-edit-recipe.php?recipe-id=<?php echo $recipe['id'] ?>">Edit</a> | <a href="/delete-recipe.php?recipe-id=<?php echo $recipe['id'] ?>">Delete</a>
+            </div>
+        <?php } ?>
     </div>
-<?php } ?>
-
+</main>
 
 <?php include('html/bottomHTML.php') ?>
 
